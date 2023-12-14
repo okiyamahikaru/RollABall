a9f9eba95a76dcce760c0a6021af79603c79585f
沖山輝 <vga29-ac220017@sankogakuen.jp>
2023-12-14 15:26:23 +0900

    テスト

---
+++ Assets/Scripts/RollABall/InGame/BallInput.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class BallInput : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // 新しいInput Systemを使用して"W"キーの入力を検知
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            Debug.Log("Wキーが押されました!");
+        }
+        // Wキーが押され続けている間
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("Wキーが押され続けてます!");
+        }
+        // Wキーが離された瞬間だけ評価する
+        if (Keyboard.current.wKey.wasReleasedThisFrame)
+        {
+            Debug.Log("Wキーが離されました!");
+        }
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Debug.Log($"マウスの座標: {mousePosition}");
+
+        //演習
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("Wキーが押され続けてます!");
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("Aキーが押され続けてます!");
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("Sキーが押され続けてます!");
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("Dキーが押され続けてます!");
+        }
+    }
+    }
---
+++ Assets/Scripts/RollABall/InGame/Bullet.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+public class Bullet : MonoBehaviour
+{
+    private float destroyTime = 5f;
+    void Update()
+    {
+        destroyTime -= Time.deltaTime;
+        if (destroyTime < 0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject != null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
